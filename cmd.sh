5970be2e15595047c901c1757c360792b7ec0e6f
Krzysztof Palcowski <kpalcowski@iterato.rs>
2018-06-07 11:20:23 +0200

    DB_CONNECTION_CHECK

--- cmd.sh
+++ cmd.sh
@@ -1,1 +1,14 @@
+if [ -z ${DB_CONNECTION_CHECK+x} ];
+  then 
+    echo "DB_CONNECTION_CHECK is not set. Skipping connection test";
+  else
+    until flyway-4.0/flyway validate -url=$DB_URL -user=$DB_USER -password=$DB_PASSWORD;
+      do
+        echo "DB is unavailable - sleeping";
+        sleep 1
+      done 
+      echo "DB is up and running";
+
+fi
+
 flyway-4.0/flyway $FLYWAY_CMD -url=$DB_URL -user=$DB_USER -password=$DB_PASSWORD -locations=filesystem:/sql
