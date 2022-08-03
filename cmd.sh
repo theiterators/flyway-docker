06f6d7677cfeefd29d5eb425f7edebde63856638
Krzysztof Palcowski <kpalcowski@iteratorshq.com>
2022-08-03 08:31:40 +0200

    change validate to invo command

--- cmd.sh
+++ cmd.sh
@@ -2,7 +2,7 @@ if [ -z ${DB_CONNECTION_CHECK+x} ];
   then 
     echo "DB_CONNECTION_CHECK is not set. Skipping connection test";
   else
-    until flyway-9.0.4/flyway validate -url=$DB_URL -user=$DB_USER -password=$DB_PASSWORD;
+    until flyway-9.0.4/flyway info -url=$DB_URL -user=$DB_USER -password=$DB_PASSWORD;
       do
         echo "DB is unavailable - sleeping";
         sleep 1
